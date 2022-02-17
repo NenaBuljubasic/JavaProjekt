08f9ade1682b828202de82044247d67be8b3361f
NenaBuljubasic <80649803+NenaBuljubasic@users.noreply.github.com>
2022-02-17 22:55:19 +0100

    Update FXML2Controller.java

--- src/FXML2Controller.java
+++ src/FXML2Controller.java
@@ -1,66 +1,266 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.LineChart;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.chart.XYChart.Series;
-import javafx.scene.control.Button;
-
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 /**
  * FXML Controller class
  *
- * @author gabrijela
+ * @author nenab
  */
-public class FXML2Controller implements Initializable {
+public class FXMLController implements Initializable {
 
     /**
      * Initializes the controller class.
      */
+    TextAnimator textAnimator;
+    TextAnimator textAnimator1;
+    Cekanje cekanje;
+    Thread thread;
+    Thread thread1;
+
+
+   
+    public Polinomi pol;
     @FXML
-    private LineChart graf;
+    private Label standL;
+    
+    @FXML
+    private Label rekL;
+    
+    @FXML 
+    private Label iterL;
+    
+    @FXML 
+    private Label standJNIL;
+    
+    @FXML
+    private BarChart grafic;
+    @FXML 
+    private TextField dimenzijaT;
+    
+    @FXML 
+    private TextArea ispisZaA;
+    
+    @FXML
+    private TextArea ispisZaB;
+    
+    @FXML 
+    private TextArea ispisZaC;
+    
+    
+    Complex[] c;
+    String umnozak,a,b;
+    long m1, m2, m3, m4;
+    BazaPodataka baza;
+    UBazi element;
+    TextOutput textOutput, textOutput1;
+    int dimenzija;
    
     @FXML
-    private Button gumb;
-    
-    @FXML
-    public void crtajPoGrafu(ActionEvent event)throws InterruptedException{
-        BazaPodataka baza = new BazaPodataka();
-        baza.stvoriBazu("baza4");
-        baza.stvoriStol();
-        List<UBazi> lista = baza.dohvatiIzBaze();
-        lista.sort(new Komparator());
+    void postaviDimenziju(ActionEvent event) throws InterruptedException {
+        c = null;
+        a = "";
+        b = "";
+        umnozak  = "";
         
-        Series standardni = new XYChart.Series<Number, Number>();
-        standardni.setName("Standardno množenje");
-        Series rekurzivni = new XYChart.Series<Number, Number>();
-        rekurzivni.setName("Rekurzivni FFT");
-        Series iterativni = new XYChart.Series<Number, Number>();
-        iterativni.setName("Iterativni FFT");
-        Series standardniJNI = new XYChart.Series<Number, Number>();
-        standardniJNI.setName("Standardni (JNI)");
+        standL.setText("");
+        rekL.setText("");
+        iterL.setText("");
+        ispisZaC.setVisible(false);
+        ispisZaA.setText("");
+        ispisZaB.setText("");
         
-        for(UBazi el:lista){
-            standardni.getData().add(new XYChart.Data(String.valueOf(el.dimenzija), el.standardni));
-            rekurzivni.getData().add(new XYChart.Data(String.valueOf(el.dimenzija), el.rekFFT));
-            iterativni.getData().add(new XYChart.Data(String.valueOf(el.dimenzija), el.iterFFT));
-            standardniJNI.getData().add(new XYChart.Data(String.valueOf(el.dimenzija), el.standJNI));
-        }
-        graf.getData().add(standardni);
-        graf.getData().add(rekurzivni);
-        graf.getData().add(iterativni);
-        graf.getData().add(standardniJNI);
+        dimenzija = Integer.parseInt(dimenzijaT.getCharacters().toString())+1;
+        System.out.println(dimenzija);
+        
+        Polinomi p = new Polinomi(dimenzija);
+        pol = p;
+        a = p.ispisPolinoma(p.prvi_polinom);
+        b = p.ispisPolinoma(p.drugi_polinom);
+        
+        
+        /*---------------racunanje ffta i mjerenje vremena--------------*/
+        
+        Dretva prva_dretva = new Dretva(p, 1);
+        Thread a1 = new Thread(prva_dretva);
+        
+        Dretva druga_dretva = new Dretva(p, 2);
+        Thread a2 = new Thread(druga_dretva);
+        
+        Dretva treca_dretva = new Dretva(p, 3);
+        Thread a3 = new Thread(treca_dretva);
+        
+        Dretva cetvrta_dretva = new Dretva(p, 4);
+        Thread a4 = new Thread(cetvrta_dretva);
+        
+        a2.start();
+        a3.start();
+        a1.start();
+        a4.start();
+        a1.join();
+        a2.join();
+        a3.join();
+        a4.join();
+        
+        c = prva_dretva.vratiRjesenje();
+        long vrijeme = prva_dretva.vratiVrijeme();
+        System.out.println("Vrijeme prvog: " + vrijeme);        
+        m1 = vrijeme;
+        standL.setText(m1 + " ms");
+        long vrijeme2 = druga_dretva.vratiVrijeme();
+        System.out.println("Vrijeme drugog: " + vrijeme2);
+        m2 = vrijeme2;
+        rekL.setText(m2 + " ms");
+        long vrijeme3 = treca_dretva.vratiVrijeme();
+        System.out.println("Vrijeme drugog: " + vrijeme3);
+        m3 = vrijeme3;
+        iterL.setText(m3 + " ms");
+        
+        long vrijeme4 = cetvrta_dretva.vratiVrijeme();
+        System.out.println("Vrijeme cetvrtog: " + vrijeme4);
+        m4 = vrijeme4;
+        standJNIL.setText(m4 + " ms");
+        
+        stvoriGraf();
+        element = new UBazi(Polinomi.id, p.dimenzija, (int)m1, (int)m2, (int)m3, (int)m4);
+        System.out.println(element);
+        baza.dodaj(element);
     }
+    void stvoriGraf(){
+        //grafic.getData().clear();
+        final  String stand = "Standardno množenje";
+        final  String rek = "Rekurzivni FFT";
+        final String iter = "Iterativni FFT";     
+        final String standJNI = "Standardni (JNI)";     
+        XYChart.Series<String, Number> serije = new XYChart.Series<>();
+        serije.setName(String.valueOf(dimenzija));
+        serije.getData().add(new XYChart.Data<>(stand, m1));
+        serije.getData().add(new XYChart.Data<>(rek, m2));
+        serije.getData().add(new XYChart.Data<>(iter, m3));
+        serije.getData().add(new XYChart.Data<>(standJNI, m4));
+        
+        grafic.getData().addAll(serije);
+    }
+    @FXML
+    void stvoriGrafIzBaze(ActionEvent event) throws InterruptedException, IOException{
+         Stage secondStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("FXML2.fxml"));
+        Scene scene = new Scene(root,657,477);
+        secondStage.setScene(scene);
+        secondStage.show();
+    }
+    @FXML
+    void pokreniRacunanje(ActionEvent event) throws InterruptedException {
+
+        String s = "";
+        for (int i=0;i<c.length;i++)
+        {
+            if(c[i].real == 0 && c[i].imag == 0)
+                continue;
+            if(i == 0)
+                s += c[i].toString() + " + ";
+            else
+            {
+                if(!c[i].toString().equals(""))
+                {
+                    String konacni = c[i].toString() + "x^" + i +" + ";
+                    s += konacni;
+                }
+               
+            }
+        }
+        
+
+        
+        umnozak = "C(x) = ";
+        for( int i = 0; i < s.length()-2; i++)
+            umnozak += s.charAt(i);
+        
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(umnozak);
+
+        
+        
+        textAnimator = new TextAnimator("A(x) = "+a,5,textOutput,ispisZaC);
+        textAnimator1 = new TextAnimator("B(x) = "+b,5,textOutput1,ispisZaC);
+        
+        thread = new Thread(textAnimator);  
+        thread.start();
+        thread1 = new Thread(textAnimator1);
+        thread1.start();
+        
+        Thread cekanje1 = new Thread(cekanje);
+        cekanje1.start();
+
+        cekanje = new Cekanje(ispisZaB, ispisZaC, "");
+        ispisZaC.setText(umnozak);
+        
+       
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        stvoriGraf();
         
-    }    
-    
+        baza = new BazaPodataka();
+        baza.stvoriBazu("baza4");
+        baza.stvoriStol();
+        ispisZaC.setVisible(false);
+
+        
+        ispisZaA.setWrapText(true);
+        ispisZaB.setWrapText(true);
+        ispisZaC.setWrapText(true);
+        textOutput = new TextOutput(){
+            @Override
+            public void writeText(String text) {
+                Platform.runLater(new Runnable(){
+                    @Override
+                    public void run() {
+                        ispisZaA.setText(text);
+                    }
+                    
+                });
+            }
+            
+        };
+        textOutput1 = new TextOutput(){
+            @Override
+            public void writeText(String text) {
+                Platform.runLater(new Runnable(){
+                    @Override
+                    public void run() {
+                        ispisZaB.setText(text);                        
+                    }
+                    
+                });
+            }
+            
+        };
+        
+    }  
 }
+
